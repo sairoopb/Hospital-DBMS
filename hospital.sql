f184c50887ccdf0f99a0540a7ff051d431435f3a
Sairoop Bodepudi <sairoopb@gmail.com>
2021-03-13 18:28:35 +0530

    Initial Commit

---
+++ hospital.sql
@@ -0,0 +1,152 @@
+DROP DATABASE IF EXISTS hospital;
+CREATE DATABASE hospital;
+USE hospital;
+
+-- 1. Patient Entity
+
+DROP TABLE IF EXISTS `Patient`;
+CREATE TABLE `Patient` (
+    `patient_id` int NOT NULL,
+    `first_name` varchar(30),
+    `last_name` varchar(30),
+    `contact_no` bigint,
+    `address` varchar(50),
+    PRIMARY KEY (`patient_id`)
+);
+
+-- 2. Room Entity
+
+DROP TABLE IF EXISTS `Room`;
+CREATE TABLE `Room` (
+    `room_no` int NOT NULL,
+    `name` varchar(30),
+    `availability` bit,
+    PRIMARY KEY (`room_no`)
+);
+
+--  3. Doctor Entity
+
+DROP TABLE IF EXISTS `Doctor`;
+CREATE TABLE `Doctor` (
+    `doctor_id` int NOT NULL,
+    `first_name` varchar(30),
+    `last_name` varchar(30),
+    `contact_no` bigint,
+    `address` varchar(50),
+    `salary` int,
+    `specialization` varchar(30),
+    `role` varchar(30),
+    `room_no`int NOT NULL,
+    PRIMARY KEY (`doctor_id`),
+    FOREIGN KEY (`room_no`) REFERENCES `Room` ( `room_no` ) ON DELETE CASCADE
+);
+
+-- 4. Medicine Entity
+
+DROP TABLE IF EXISTS `Medicine`;
+CREATE TABLE `Medicine` (
+    `medicine_id` int NOT NULL,
+    `inventory_quantity` int,
+    `name` varchar(30),
+    `price` int,
+    PRIMARY KEY (`Medicine_id`)
+);
+
+-- 5. Nurse entity
+
+DROP TABLE IF EXISTS `Nurse`;
+CREATE TABLE `Nurse` (
+    `nurse_id` int NOT NULL,
+    `first_name` varchar(30),
+    `last_name` varchar(30),
+    `contact_no` bigint,
+    `address` varchar(50),
+    `salary` int,
+    PRIMARY KEY (`nurse_id`)
+);
+
+-- 6. Bill Entity
+
+DROP TABLE IF EXISTS `Bill`;
+CREATE TABLE `Bill` (
+    `bill_number` int NOT NULL,
+    `bill_type` int,
+    `total` int,
+    `subtotal` int,
+    `taxes` int,
+    `paid` bit,
+    PRIMARY KEY (`bill_number`)
+);
+
+-- 7. Employee Entity
+
+DROP TABLE IF EXISTS `Employee`;
+CREATE TABLE `Employee` (
+    `employee_id` int NOT NULL,
+    `first_name` varchar(30),
+    `last_name` varchar(30),
+    `contact_no` bigint,
+    `occupation` varchar(30),
+    `address` varchar(50),
+    `salary` int,
+    PRIMARY KEY (`employee_id`)
+);
+
+-- 8. Prescription Entity
+
+DROP TABLE IF EXISTS `Prescription`;
+CREATE TABLE `Prescription` (
+    `prescription_id` int NOT NULL,
+    PRIMARY KEY (`prescription_id`)
+);
+
+-- 9. Diagnosis Entity
+
+DROP TABLE IF EXISTS `Diagnosis`;
+CREATE TABLE `Diagnosis` (
+    `diagnosis_id` int NOT NULL,
+    `results` varchar(100),
+    PRIMARY KEY (`diagnosis_id`)
+);
+
+-- 10. Treatment Entity
+
+DROP TABLE IF EXISTS `Treatment`;
+CREATE TABLE `Treatment` (
+    `treatment_id` int NOT NULL,
+    `start_date` Date,
+    `end_date` Date,
+    `details` varchar(100),
+    PRIMARY KEY (`treatment_id`)
+);
+
+-- 11. Test Entity
+
+DROP TABLE IF EXISTS `Test`;
+CREATE TABLE `Test` (
+    `test_id` int NOT NULL,
+    `name` varchar(50),
+    `cost` int,
+    PRIMARY KEY (`test_id`)
+);
+
+-- 12. Procedure Entity
+
+DROP TABLE IF EXISTS `Procedure`;
+CREATE TABLE `Procedure` (
+    `procedure_id` int NOT NULL,
+    `name` varchar(50),
+    `price` int,
+    PRIMARY KEY (`procedure_id`)
+);
+
+-- 13. Patient Log Entity
+
+DROP TABLE IF EXISTS `Patient_Log`;
+CREATE TABLE `Patient_log` (
+    `patient_id` int NOT NULL,
+    `checkin` varchar(50),
+    `checkout` int,
+    PRIMARY KEY (`patient_id`),
+    FOREIGN KEY (`patient_id`) REFERENCES `Patient` ( `patient_id` ) ON DELETE CASCADE
+);
