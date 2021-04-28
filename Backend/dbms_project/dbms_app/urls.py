497ab16a55aea9f7f5633b8737a5ea341dd7b62b
BatJedi <yuvrajr4242@gmail.com>
2021-04-28 22:18:58 +0530

    Renamed things in home page

--- Backend/dbms_project/dbms_app/urls.py
+++ Backend/dbms_project/dbms_app/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.home_view,name = "home"),
-    path("appointments/<str:branch>",views.appoint_view,name = "appointments"),
-    path("diagnosis/<str:branch>",views.diag_view,name = "diagnosis"),
-    path("patients_reg/<str:branch>",views.patient_view,name = "patients_reg"),
-    path("payments/<str:branch>",views.payments_view,name = "payments"),
-    path("pharamcy/<str:branch>",views.pharmacy_view,name = "pharmacy"),
-    path("prescriptions/<str:branch>",views.prescription_view,name = "prescriptions"),
-    path("treatments/<str:branch>",views.treatment_view,name = "treatments"),
+    path("Appointments/<str:branch>",views.appoint_view,name = "appointments"),
+    path("Diagnoses/<str:branch>",views.diag_view,name = "diagnosis"),
+    path("Patient Log/<str:branch>",views.patient_view,name = "patients_reg"),
+    path("Payments/<str:branch>",views.payments_view,name = "payments"),
+    path("Pharamcy/<str:branch>",views.pharmacy_view,name = "pharmacy"),
+    path("Prescriptions/<str:branch>",views.prescription_view,name = "prescriptions"),
+    path("Treatments/<str:branch>",views.treatment_view,name = "treatments"),
 ]
