4efa23d3fc0e4a3c35d9760445b512908bcdf089
BatJedi <yuvrajr4242@gmail.com>
2021-04-28 22:19:21 +0530

    Made buttons blue and shiny. Spelling error fixed in urls.py

--- Backend/dbms_project/dbms_app/urls.py
+++ Backend/dbms_project/dbms_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("Diagnoses/<str:branch>",views.diag_view,name = "diagnosis"),
     path("Patient Log/<str:branch>",views.patient_view,name = "patients_reg"),
     path("Payments/<str:branch>",views.payments_view,name = "payments"),
-    path("Pharamcy/<str:branch>",views.pharmacy_view,name = "pharmacy"),
+    path("Pharmacy/<str:branch>",views.pharmacy_view,name = "pharmacy"),
     path("Prescriptions/<str:branch>",views.prescription_view,name = "prescriptions"),
     path("Treatments/<str:branch>",views.treatment_view,name = "treatments"),
 ]
