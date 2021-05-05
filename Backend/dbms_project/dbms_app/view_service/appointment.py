e22b3ca2536870a55032a6270ac8d095a76d6793
Sairoop Bodepudi <sairoopb@gmail.com>
2021-05-06 03:01:50 +0530

    UI Fix

--- Backend/dbms_project/dbms_app/view_service/appointment.py
+++ Backend/dbms_project/dbms_app/view_service/appointment.py
@@ -11,23 +11,35 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 def appointment_view(request,branch):
-    if request.method == "GET":
-        with connection.cursor() as cursor:
-            if 'patient_id' not in request.GET:
-                if 'doctor_id' not in request.GET or request.GET['doctor_id'] == "":
+    if branch == "show" or branch == "index":
+        if request.method == "POST":
+            with connection.cursor() as cursor:
+                if 'doctor_id' in request.POST and request.POST['doctor_id'] != "":
+                    cursor.execute("SELECT * FROM Appointment_Schedule where doctor_id = %s ORDER BY doctor_id asc, `date` asc, start_time asc", [request.POST['doctor_id']])
+                elif ('doctor_id' not in request.POST or request.POST['doctor_id'] == "") and ('specialization' in request.POST) and request.POST['specialization'] != "":
+                    cursor.execute("SELECT * FROM Appointment_Schedule WHERE specialization = %s ORDER BY doctor_id asc, `date` asc, start_time asc", [request.POST['specialization']])
+                else :
                     cursor.execute("SELECT * FROM Appointment_Schedule ORDER BY doctor_id asc, `date` asc, start_time asc")
-                else:
-                    cursor.execute("SELECT * FROM Appointment_Schedule where doctor_id = %s ORDER BY doctor_id asc, `date` asc, start_time asc", [request.GET['doctor_id']])
                 columns = [col[0] for col in cursor.description]
                 return render(request,"dbms_app/appointment.html", {"headers": columns, "data":dictfetchall(cursor)})
-            else:
-                cursor.callproc("create_appointment", [request.GET['d_id'], request.GET['patient_id'], request.GET['bookDate'], 0])
-                cursor.execute("SELECT @_create_appointment_3")
-                if (cursor.fetchone()[0] == 1):
-                    cursor.execute("SELECT * FROM Appointment_Schedule ORDER BY doctor_id asc, `date` asc, start_time asc")
-                    columns = [col[0] for col in cursor.description]
-                    return render(request,"dbms_app/appointment.html", {"headers": columns, "data":dictfetchall(cursor), "createFlag" : True})
+        else:
+            with connection.cursor() as cursor:
                 cursor.execute("SELECT * FROM Appointment_Schedule ORDER BY doctor_id asc, `date` asc, start_time asc")
                 columns = [col[0] for col in cursor.description]
-                return render(request,"dbms_app/appointment.html", {"headers": columns, "data":dictfetchall(cursor), "errorFlag" : True})
-                
+                return render(request,"dbms_app/appointment.html", {"headers": columns, "data":dictfetchall(cursor)})
+    elif branch == "create":
+        if request.method == "POST":
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM Doctor WHERE doctor_id = %s", [request.POST['d_id']])
+                if cursor.rowcount == 0:
+                    return render(request, "dbms_app/create_appointment.html",{"error" : True})
+                cursor.execute("SELECT * FROM Patient WHERE patient_id = %s", [request.POST['patient_id']])
+                if cursor.rowcount == 0:
+                    return render(request, "dbms_app/payments.html",{"error" : True})
+                cursor.callproc("create_appointment", [request.POST['d_id'], request.POST['patient_id'], request.POST['bookDate'], 0])
+                cursor.execute("SELECT @_create_appointment_3")
+                if (cursor.fetchone()[0] == 1):
+                    return render(request,"dbms_app/create_appointment.html", {"createFlag" : True})
+                return render(request,"dbms_app/create_appointment.html", {"errorFlag" : True})
+        else:
+            return render(request, "dbms_app/create_appointment.html")
