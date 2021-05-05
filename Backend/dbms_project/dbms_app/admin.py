c593f3a089d9a5bc1c6527706b8c6aaca3022077
SIDDHARTH RAHUL SHAH <111801040@smail.iitpkd.ac.in>
2021-05-06 01:18:27 +0530

    signup and login created.

--- Backend/dbms_project/dbms_app/admin.py
+++ Backend/dbms_project/dbms_app/admin.py
@@ -3,12 +3,10 @@ from django.apps import apps
 from .models import User
 from django.contrib.auth.admin import UserAdmin
 
-# admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+
 models = apps.get_models()
 
 
 for model in models:
-    if "dbms_app.models" in str(model) and "dbms_app.models.User" not in str(model):
-        print(type(type(model)),type(model))
+    if "dbms_app.models" in str(model):
         admin.site.register(model)
